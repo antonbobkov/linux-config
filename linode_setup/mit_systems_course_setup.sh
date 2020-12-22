a7f9d57400b4f30f61fce4f3a66749e7641ab0e1
Anton Bobkov <antonbobkov@users.noreply.github.com>
2020-12-21 17:51:19 -0800

    Create mit_systems_course_setup.sh

---
+++ linode_setup/mit_systems_course_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone git@github.com:antonbobkov/mit-systems-course.git ~/mit-systems-course
+sudo apt-get install golang -y
+
+# Main course page https://pdos.csail.mit.edu/6.824/schedule.html
+
+# mapreduce lab
+# https://pdos.csail.mit.edu/6.824/labs/lab-mr.html
+git clone git://g.csail.mit.edu/6.824-golabs-2020 ~/6.824
