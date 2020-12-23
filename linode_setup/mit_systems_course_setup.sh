0b20160b31de00353deb3f4bba487262ed9124ff
Anton Bobkov <antonbobkov@users.noreply.github.com>
2020-12-23 15:43:06 -0800

    Update mit_systems_course_setup.sh

--- linode_setup/mit_systems_course_setup.sh
+++ linode_setup/mit_systems_course_setup.sh
@@ -8,3 +8,18 @@ sudo apt-get install golang -y
 # mapreduce lab
 # https://pdos.csail.mit.edu/6.824/labs/lab-mr.html
 git clone git://g.csail.mit.edu/6.824-golabs-2020 ~/6.824
+
+
+# ~/.emacs
+
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "https://melpa.org/packages/")
+   t))
+   
+# in emacs
+# M-x package-refresh-contents and then M-x package-install RET go-mode RET
